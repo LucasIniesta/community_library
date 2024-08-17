b948cf4055adf12c7edcf47b58fd913bffd602ba
LucasIniesta <l.iniesta.94@gmail.com>
2024-08-16 22:44:36 -0300

    feat: CRUD complete inserted

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,28 @@
 import {Router} from 'express'
 import userController from '../controllers/user.controllers.js'
+import { validate, validateUserId } from '../middlewares/validation.middlewares.js'
+import {userSchema} from '../schema/user.schema.js'
 
 const router = Router()
 
-router.post('/users', userController.createUserController)
+router.post('/users', 
+    validate(userSchema), 
+    userController.createUserController
+)
+
+router.get('/users', userController.findAllUsersController)
+router.get('/users/:id', 
+    validateUserId, 
+    userController.findUserByIdController
+)
+router.put('/users/:id', 
+    validate(userSchema),
+    validateUserId, 
+    userController.updateUserController
+)
+router.delete('/users/:id', 
+    validateUserId, 
+    userController.deleteUserController
+)
 
 export default router
